0a081b7b2cc41c910e4ada3d34bffe503a8f7120
Abdullah Naser <abdnaser.exe@gmail.com>
2020-09-10 18:58:58 +0600

    Dynamic routing fixed

--- src/App.js
+++ src/App.js
@@ -3,21 +3,20 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home'
 import Login from './components/login'
 import Dashboard from './components/dashboard/dashboardLayout'
+import RouteTest from './components/RouteTest'
+
 function App() {
   return (
     <div className="">
-
         <Router>
-   
           <div className="">
-          
             <Switch>
               <Route exact path='/dashboard' component={Dashboard} />
+              <Route exact path='/dashboard/:comp' component={Dashboard} />
               <Route exact path='/login' component={Login} />
               <Route exact path='/' component={Home} />
             </Switch>
           </div>
-       
         </Router>
     </div>
   );
---
+++ src/components/RouteTest.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DashboardLayout from './dashboard/dashboardLayout'
+
+const RouteTest = () => {
+  return (
+    <div>
+      <DashboardLayout/>
+    </div>
+  )
+}
+
+export default RouteTest;
